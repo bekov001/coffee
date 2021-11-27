aa74e7262638ea81ff6cbc8c04464a93031adfcb
Anuar <ava200ava@gmail.com>
2021-11-27 20:57:16 +0600

    THEEND

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem, QDialog
 from PyQt5 import QtWidgets
 
+from UI.main import Ui_Form
+from UI.addEditCoffeeForm import Ui_Dialog
 
-class Coffee(QWidget):
+
+class Coffee(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi("main.ui", self)
+        self.setupUi(self)
         self.setWindowTitle("Кофе")
-        self.con = sqlite3.connect("main.sqlite3")
+        self.con = sqlite3.connect("data/main.sqlite3")
         self.table = "coffee"
         self.columns = ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки']
         self.tables = ["SORT", "ROAST", "MILLED", "DESCRIPTION", "PRICE", "VOLUME"]
@@ -29,12 +32,12 @@ class Coffee(QWidget):
             if self.valid.exec():
                 query = f"""UPDATE {self.table} SET """ + \
                         ",\n".join(
-                            [" = ".join(i if i[1].isdigit() else (i[0], f"'{i[1]}'")) for i in zip(self.tables, items)]) + \
+                            [" = ".join(i if i[1].isdigit() else (i[0], f"'{i[1]}'")) for i in zip(self.tables, [str(i) for i in self.valid.get_values()])]) + \
                         f" WHERE ID = {items[0]}"
-                print(query)
                 cur = self.con.cursor()
                 cur.execute(query)
                 self.con.commit()
+                self.result()
 
     def delete_coffee(self):
         choose = self.tableWidget.selectedItems()
@@ -47,8 +50,9 @@ class Coffee(QWidget):
             self.result()
 
     def add_coffee(self):
+        self.valid.clear()
         if self.valid.exec():
-            roast, volume, price, sort, type_coffee, desc = self.valid.get_values()
+            sort, roast, type_coffee, desc, price, volume = self.valid.get_values()
             cur = self.con.cursor()
             cur.execute("""INSERT INTO coffee(SORT, ROAST, MILLED, DESCRIPTION, 
             PRICE, VOLUME) VALUES(?, ?, ?, ?, ?, ?)""",
@@ -56,67 +60,37 @@ class Coffee(QWidget):
             self.con.commit()
             self.result()
 
-    def run(self, data=False):
-        try:
-            if data:
-                self.valid.lineEdit.setText(data[0])
-                self.valid.lineEdit_2.setText(data[1])
-                self.valid.lineEdit_3.setText(data[2])
-                self.valid.box.setCurrentText(data[3])
-                print(data)
-            if self.valid.exec():
-                if not self.valid.lineEdit_2.text() or 1000 > int(
-                        self.valid.lineEdit_2.text()) or int(
-                        self.valid.lineEdit_2.text()) > 2021:
-                    self.valid.label_5.setText("Некорректный год")
-                    self.run(data)
-                elif not self.valid.lineEdit.text() or int(self.valid.lineEdit.text()) < 10:
-                    self.valid.label_5.setText("Неправильное продолжительность фильма")
-                    self.run(data)
-                else:
-                    cur = self.con.cursor()
-                    query = f"""INSERT INTO films (title, year, genre, duration)
-                    VALUES('{self.valid.lineEdit_3.text()}', 
-                    {self.valid.lineEdit_2.text()}, 
-                    (select id from genres where title = '{self.valid.box.currentText()}'), 
-                    {self.valid.lineEdit.text()})"""
-                    if data:
-                        query = f"""UPDATE films SET title = '{self.valid.lineEdit_3.text()}', year = {self.valid.lineEdit_2.text()}, genre = (select id from genres where title = '{self.valid.box.currentText()}'),
-                        duration =  {self.valid.lineEdit.text()}
-                                    WHERE ID = {data[-1]}"""
-                    print(query)
-                    cur.execute(query)
-                    self.con.commit()
-                    self.result("films", ["ID", "Название", "Год", "Жанр",
-                                          "Продолжиельность"], True)
-        except Exception as ex:
-            self.valid.label_5.setText("Неверная форма")
-            print(ex)
-            self.run(data)
-
     def result(self):
-        cur = self.con.cursor()
-        # Получили результат запроса, который ввели в текстовое поле
-        result = cur.execute("SELECT * FROM " + self.table).fetchall()
+        try:
+            cur = self.con.cursor()
+            # Получили результат запроса, который ввели в текстовое поле
+            result = cur.execute("SELECT ID, SORT, ROAST, MILLED, DESCRIPTION, PRICE, VOLUME FROM " + self.table).fetchall()
+            # Заполнили размеры таблицы
+            self.tableWidget.setRowCount(len(result))
+            # Если запись не нашлась, то не будем ничего делать
+            self.tableWidget.setColumnCount(len(result[0]))
+            self.tableWidget.setHorizontalHeaderLabels(self.columns)
+            # Заполнили таблицу полученными элементами
+            for i, elem in enumerate(result):
+                for j, val in enumerate(elem):
+                    self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+            header = self.tableWidget.horizontalHeader()
+            header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
+        except IndexError:
+            pass
 
-        # Заполнили размеры таблицы
-        self.tableWidget.setRowCount(len(result))
-        # Если запись не нашлась, то не будем ничего делать
-        self.tableWidget.setColumnCount(len(result[0]))
-        self.tableWidget.setHorizontalHeaderLabels(self.columns)
-        # Заполнили таблицу полученными элементами
-        for i, elem in enumerate(result):
-            for j, val in enumerate(elem):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
-        header = self.tableWidget.horizontalHeader()
-        header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
 
-
-class PopUp(QDialog):
+class PopUp(QDialog, Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Кофе")
-        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.setupUi(self)
+
+    def clear(self):
+        self.sort_line.clear()
+        self.desc_edit.clear()
+        self.price_box.clear()
+        self.volume_box.clear()
 
     def set_values(self, sort, roast, milled, desc, price, volume):
         self.sort_line.setText(sort)
@@ -134,7 +108,7 @@ class PopUp(QDialog):
         type_coffee = self.type_box.currentIndex()
         desc = self.desc_edit.toPlainText()
 
-        return roast, volume, price, sort, type_coffee, desc
+        return sort, roast, type_coffee, desc, price, volume
 
 
 def except_hook(cls, exception, traceback):
